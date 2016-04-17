98bf1fb3f9488484d3e2f4545566fc7dda72539f
Benedict Wong <ObsessiveOrange@users.noreply.github.com>
2016-04-17 17:43:10 -0400

    Added new RunFormatter script  (#3)
    * Added new RunFormatter script that prints any golint/govet errors, and auto-formats the source code
    
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>
    
    * Updated the Git hook to use RunFormatter instead of CheckFormatting
    
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>
    
    PR signed-off by Joel Shapiro

---
+++ RunFormatter.sh
@@ -0,0 +1,57 @@
+     #!/usr/bin/env bash
+
+PACKAGES=$(go list ./... | grep -v /vendor/)
+FILES=$(find . -type f -name '*.go' -not -path "./vendor/*")
+
+RESULT_GOLINT="  PASSED: GoLint"
+RESULT_GOVET="  PASSED: GoVet"
+RESULT_GOFMT="  PASSED: GoFmt"
+RESULT_GOIMPORTS="  PASSED: Imports"
+
+STATUS=0
+
+printf -- "Checking Formatting:\n--------------------------------------------------------------------------------\n"
+
+# Check GoLint
+if [[ "$(for p in $PACKAGES; do golint $p 2>&1; done)" ]]; then
+    printf -- "GoLint errors:\n"
+    RESULT_GOLINT="->Failed: GoLint"
+    for p in $PACKAGES; do golint $p 2>&1; done
+    printf -- "\n"
+    STATUS=1
+fi
+
+# Check GoVet
+if [[ "$(go vet $PACKAGES 2>&1)" ]]; then
+    printf -- "GoVet errors:\n"
+    RESULT_GOVET="->Failed: GoVet"
+    go vet $PACKAGES 2>&1
+    printf -- "\n"
+    STATUS=1
+fi
+
+# Check GoFmt
+if [[ "$(gofmt -s -l $FILES 2>&1)" ]]; then
+    printf -- "GoFmt reformatting code.\n"
+    gofmt -s -w $FILES 2>&1
+    RESULT_GOFMT="  REFORMATTED: GoFmt"
+fi
+
+# Check GoImports
+if [[ "$($GOPATH/bin/goimports -l $FILES 2>&1)" ]]; then
+    printf -- "GoImports reformatting code.\n"
+    $GOPATH/bin/goimports -w $FILES 2>&1
+    RESULT_GOIMPORTS="  REFORMATTED: GoImports"
+fi
+
+printf -- "SUMMARY:\n"
+printf -- "--------------------------------------------------------------------------------\n"
+
+echo $RESULT_GOLINT
+echo $RESULT_GOVET
+echo $RESULT_GOFMT
+echo $RESULT_GOIMPORTS
+
+printf -- "--------------------------------------------------------------------------------\n"
+
+exit $STATUS
