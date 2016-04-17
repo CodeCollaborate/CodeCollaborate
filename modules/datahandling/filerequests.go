0db80813020a5070619817196780e43ae4694929
loganga <loganga@rose-hulman.edu>
2016-04-17 17:41:08 -0400

    Fixed some method signature issues.

--- modules/datahandling/filerequests.go
+++ modules/datahandling/filerequests.go
@@ -21,6 +21,46 @@ func initFileRequests() {
 		return p, err
 	}
 
+	requestMap["FileRename"] = func(req AbstractRequest) (Request, error) {
+		p := new(fileRenameRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["FileMove"] = func(req AbstractRequest) (Request, error) {
+		p := new(fileMoveRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["FileDelete"] = func(req AbstractRequest) (Request, error) {
+		p := new(fileDeleteRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["FileChange"] = func(req AbstractRequest) (Request, error) {
+		p := new(fileChangeRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["FilePull"] = func(req AbstractRequest) (Request, error) {
+		p := new(filePullRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
 	fileRequestsSetup = true
 }
 
@@ -34,6 +74,64 @@ type fileCreateRequest struct {
 }
 
 func (p fileCreateRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
-	fmt.Println(p.Name)
+	fmt.Printf("Recieved file create request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// File.Rename
+type fileRenameRequest struct {
+	FileID  string
+	NewName string
+	AbstractRequest
+}
+
+func (p fileRenameRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	fmt.Printf("Recieved file rename request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// File.Move
+type fileMoveRequest struct {
+	FileID  string
+	NewPath string
+	AbstractRequest
+}
+
+func (p fileMoveRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	fmt.Printf("Recieved file move request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// File.Delete
+type fileDeleteRequest struct {
+	FileID string
+	AbstractRequest
+}
+
+func (p fileDeleteRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	fmt.Printf("Recieved file delete request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// File.Change
+type fileChangeRequest struct {
+	FileID  string
+	Changes []string
+	AbstractRequest
+}
+
+func (p fileChangeRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	fmt.Printf("Recieved file change request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// File.Pull
+type filePullRequest struct {
+	FileID string
+	AbstractRequest
+}
+
+func (p filePullRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	fmt.Printf("Recieved file pull request from %s\n", p.SenderID)
 	return nil, nil, nil
 }
--- modules/datahandling/projectrequests.go
+++ modules/datahandling/projectrequests.go
@@ -13,8 +13,83 @@ func initProjectRequests() {
 		return
 	}
 
+	// This isn't an ideal way of populating the map since there is so much duplicate code, but extracting this
+	// requires some kind of restructuring of the request hierarchy, which can come later because we can't come up
+	// with an obvious way to do it.
+	requestMap["ProjectCreateRequest"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectCreateRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectRename"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectRenameRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectGetPermissionsConstants"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectGetPermissionConstantsRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectGrantPermissions"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectGrantPermissionsRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectRevokePermissions"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectRevokePermissions)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectGetOnlineClients"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectGetOnlineClients)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
 	requestMap["ProjectLookup"] = func(req AbstractRequest) (Request, error) {
 		p := new(projectLookupRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectGetFiles"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectGetFilesRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectSubscribe"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectSubscribeRequest)
+		p.AbstractRequest = req
+		rawData := p.Data
+		err := json.Unmarshal(rawData, &p)
+		return p, err
+	}
+
+	requestMap["ProjectDelete"] = func(req AbstractRequest) (Request, error) {
+		p := new(projectDeleteRequest)
 		p.AbstractRequest = req
 		rawData := p.Data
 		err := json.Unmarshal(rawData, &p)
@@ -25,14 +100,124 @@ func initProjectRequests() {
 }
 
 // Project.Create
+type projectCreateRequest struct {
+	Name string
+	AbstractRequest
+}
+
+func (p projectCreateRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project create request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.Rename
+type projectRenameRequest struct {
+	ProjectID string
+	NewName   string
+	AbstractRequest
+}
+
+func (p projectRenameRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project rename request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.GetPermissionConstants
+type projectGetPermissionConstantsRequest struct {
+	AbstractRequest
+}
+
+func (p projectGetPermissionConstantsRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project get permissions constants request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.GrantPermissions
+type projectGrantPermissionsRequest struct {
+	ProjectID       string
+	GrantUsername   string
+	PermissionLevel int
+	AbstractRequest
+}
+
+func (p projectGrantPermissionsRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project grant permissions request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.RevokePermissions
+type projectRevokePermissions struct {
+	ProjectID      string
+	RevokeUsername string
+	AbstractRequest
+}
+
+func (p projectRevokePermissions) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project revoke permissions request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.GetOnlineClients
+type projectGetOnlineClients struct {
+	ProjectID string
+	AbstractRequest
+}
+
+func (p projectGetOnlineClients) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project get online clients request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
 
 // Project.Lookup
 type projectLookupRequest struct {
-	ProjectIds []int64
+	ProjectIDs []int64
 	AbstractRequest
 }
 
 func (p projectLookupRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
-	fmt.Println(p.ProjectIds)
+	// TODO
+	fmt.Printf("Recieved project lookup request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.GetFiles
+type projectGetFilesRequest struct {
+	ProjectID string
+	AbstractRequest
+}
+
+func (p projectGetFilesRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved get project files request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.Subscribe
+type projectSubscribeRequest struct {
+	ProjectID string
+	AbstractRequest
+}
+
+func (p projectSubscribeRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project subscribe request from %s\n", p.SenderID)
+	return nil, nil, nil
+}
+
+// Project.Delete
+type projectDeleteRequest struct {
+	ProjectID string
+	AbstractRequest
+}
+
+func (p projectDeleteRequest) Process() (response *ServerMessageWrapper, notification *ServerMessageWrapper, err error) {
+	// TODO
+	fmt.Printf("Recieved project delete request from %s\n", p.SenderID)
 	return nil, nil, nil
 }
