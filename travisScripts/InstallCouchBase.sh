be2d46b3ce7b388be56b442d3d450d2307a9e975
Benedict Wong <bennydictwong@gmail.com>
2016-05-02 18:51:07 -0400

    Reduced sleep time

--- travisScripts/InstallCouchBase.sh
+++ travisScripts/InstallCouchBase.sh
@@ -3,6 +3,6 @@
 sudo wget http://packages.couchbase.com/releases/4.5.0-DP1/couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb
 sudo dpkg -i couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb
 sudo service couchbase-server restart
-sleep 15
+sleep 5
 /opt/couchbase/bin/couchbase-cli cluster-init -c localhost:8091 --cluster-username=Administrator --cluster-password=password --cluster-ramsize=512
 /opt/couchbase/bin/couchbase-cli bucket-create -c localhost:8091 -u Administrator -p password --bucket=documents --bucket-type=couchbase --bucket-ramsize=512 --wait
