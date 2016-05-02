269a038ac6a43c5facdca18aa593a61d09670552
Benedict Wong <bennydictwong@gmail.com>
2016-05-02 18:24:55 -0400

    Added bucket creation

--- travisScripts/InstallCouchBase.sh
+++ travisScripts/InstallCouchBase.sh
@@ -21,5 +21,6 @@ sudo iptables -L
 sudo wget http://packages.couchbase.com/releases/4.5.0-DP1/couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb
 sudo dpkg -i couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb
 sudo service couchbase-server restart
-sleep 60
+sleep 15
 /opt/couchbase/bin/couchbase-cli cluster-init -c $HOSTNAME:8091 --cluster-username=Administrator --cluster-password=password --cluster-ramsize=512
+/opt/couchbase/bin/couchbase-cli bucket-create -c $HOSTNAME:8091 -u Administrator -p password --bucket=documents --bucket-type=couchbase --bucket-ramsize=512 --wait
