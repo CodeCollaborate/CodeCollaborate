53a42a8fa7826211f0975326c1784aa6cb6b698a
Joel Shapiro <joel.shapiro7@gmail.com>
2016-08-09 18:34:28 -0500

    attempt travis caching

--- travisScripts/InstallCouchBase.sh
+++ travisScripts/InstallCouchBase.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-wget http://packages.couchbase.com/releases/4.5.0-DP1/couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb -O couchbase_server.deb
-dpkg-deb -x couchbase_server.deb $HOME
+if [ ! -f downloads/couchbase_server-4.5.0-DP1.deb ]; then
+    wget http://packages.couchbase.com/releases/4.5.0-DP1/couchbase-server-enterprise_4.5.0-DP1-ubuntu14.04_amd64.deb -O downloads/couchbase_server-4.5.0-DP1.deb
+fi
+
+dpkg-deb -x downloads/couchbase_server-4.5.0-DP1.deb $HOME
 cd $HOME/opt/couchbase
 ./bin/install/reloc.sh `pwd`
 ./bin/couchbase-server -- -noinput -detached
