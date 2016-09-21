c9a23319a7717136995ec3df505b6dd22b320567
Benedict Wong <bennydictwong@gmail.com>
2016-09-21 02:57:32 -0400

    Removed unnecessary method
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>

--- modules/datahandling/testutils_test.go
+++ modules/datahandling/testutils_test.go
@@ -2,21 +2,10 @@ package datahandling
 
 import (
 	"testing"
-
 	"time"
 
 	"github.com/CodeCollaborate/Server/modules/config"
 )
-
-func configSetupUnauthenticated(t *testing.T) {
-	config.SetConfigDir("../../config")
-	err := config.InitConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	config.GetConfig().ServerConfig.DisableAuth = true
-}
 
 func configSetup(t *testing.T) {
 	config.SetConfigDir("../../config")
