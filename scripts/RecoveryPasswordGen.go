a47ac7e40d0b76ecd347df876e374d9a86f270a6
Joel Shapiro <joel.shapiro7@gmail.com>
2016-11-07 17:34:57 -0500

    add password generation abilities outside of the user regiser request

---
+++ scripts/RecoveryPasswordGen.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	password = flag.String("password", "password", "Prefix for all Docker images and volumes")
+)
+
+func main() {
+	flag.Parse()
+
+	args := os.Args[1:]
+	if len(args) > 0 && *password == "password" {
+		// didn't use flag
+		inferred := args[len(args)-1]
+		password = &inferred
+	}
+
+	fmt.Printf("generating hash for password %s: \n", string(*password))
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(*password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("ERROR: problem making hash")
+		fmt.Println(err)
+	}
+	fmt.Println(string(hashed))
+}
