744a121f4198a986df8976d8c1f5eeb96688676e
Benedict Wong <bennydictwong@gmail.com>
2016-12-03 17:20:46 -0500

    Updated server script
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>

--- scripts/server/update_server.sh
+++ scripts/server/update_server.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+printf "%-60s" "Syncing to head"
+git pull
+if [ $? -eq 0 ]; then
+    printf "%-10s\n" "OK"
+else
+    printf "%-10s\n" "FAIL"
+fi
+
+printf "%-60s" "Syncing dependencies"
+govendor sync
+if [ $? -eq 0 ]; then
+    printf "%-10s\n" "OK"
+else
+    printf "%-10s\n" "FAIL"
+fi
+
 printf "%-60s" "Stopping CodeCollaborate daemon"
 sudo service CodeCollaborate stop
 if [ $? -eq 0 ]; then
