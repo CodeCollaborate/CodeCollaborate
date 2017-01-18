3176c4cd31820be220e5f0a4f44aadaaa5be42a7
Joel Shapiro <joel.shapiro7@gmail.com>
2017-01-17 22:42:11 -0500

    add colored server log output

--- utils/consolehook.go
+++ utils/consolehook.go
@@ -1,23 +1,44 @@
 package utils
 
 import (
-	"log"
+	"os"
 
 	"github.com/Sirupsen/logrus"
 )
 
+// MakeConsoleHook creates the logrus hook to print to the terminal
+func MakeConsoleHook() *ConsoleHook {
+	consoleLog := logrus.New()
+	// use stdout instead of stderr
+	consoleLog.Out = os.Stdout
+	// text formatter w/ color if it's supported
+	consoleLog.Formatter = &logrus.TextFormatter{}
+	return &ConsoleHook{logger: consoleLog}
+}
+
 // ConsoleHook to duplicate file logs to console
-type ConsoleHook struct{}
+type ConsoleHook struct {
+	logger *logrus.Logger
+}
 
 // Fire is triggered on new log entries
 func (hook *ConsoleHook) Fire(entry *logrus.Entry) error {
-	str, err := entry.String()
-	if err != nil {
-		log.Printf("Unable to read entry: %v", err)
-		return err
+	if entry.Logger.Level >= entry.Level {
+		switch entry.Level {
+		case logrus.DebugLevel:
+			hook.logger.WithFields(entry.Data).Debug(entry.Data)
+		case logrus.InfoLevel:
+			hook.logger.WithFields(entry.Data).Info(entry.Message)
+		case logrus.WarnLevel:
+			hook.logger.WithFields(entry.Data).Warn(entry.Message)
+		case logrus.ErrorLevel:
+			hook.logger.WithFields(entry.Data).Error(entry.Message)
+		case logrus.FatalLevel:
+			hook.logger.WithFields(entry.Data).Fatal(entry.Message)
+		case logrus.PanicLevel:
+			hook.logger.WithFields(entry.Data).Panic(entry.Level)
+		}
 	}
-
-	log.Print(str)
 	return nil
 }
 
