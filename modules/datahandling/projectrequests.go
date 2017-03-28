b52fb0d4ab131882220a6de9c988bed21a6423ab
Benedict Wong <bennydictwong@gmail.com>
2017-03-27 23:32:06 -0400

    Prevent users from changing their own permissions
    Signed-off-by: Benedict Wong <bennydictwong@gmail.com>

--- modules/datahandling/projectrequests.go
+++ modules/datahandling/projectrequests.go
@@ -185,6 +185,11 @@ func (p projectGrantPermissionsRequest) process(db dbfs.DBFS) ([]dhClosure, erro
 		return []dhClosure{toSenderClosure{msg: messages.NewEmptyResponse(messages.StatusUnauthorized, p.Tag)}}, nil
 	}
 
+	// Prevent users from changing their own permissions
+	if p.SenderID == p.GrantUsername {
+		return []dhClosure{toSenderClosure{msg: messages.NewEmptyResponse(messages.StatusUnauthorized, p.Tag)}}, nil
+	}
+
 	// TODO: Add if User exists check
 
 	requestPerm, err := config.PermissionByLevel(p.PermissionLevel)
